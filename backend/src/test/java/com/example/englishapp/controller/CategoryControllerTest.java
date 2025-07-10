b78bf849382df6f361aa939636f5b0f0975836a5
Hiroki Sadakuni <h-sadakuni@ap-sys.com>
2025-07-10 09:20:11 +0900

    不要import削除

--- backend/src/test/java/com/example/englishapp/controller/CategoryControllerTest.java
+++ backend/src/test/java/com/example/englishapp/controller/CategoryControllerTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
